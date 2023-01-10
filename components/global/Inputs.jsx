05b148e931fd1f9da12110288f7439a7dcd81f38
Achilles <vishnubhupathiamudha@gmail.com>
2023-01-10 19:42:26 +0530

    push-now

--- components/global/Inputs.jsx
+++ components/global/Inputs.jsx
@@ -4,42 +4,6 @@ import Link from "next/link";
 import axios from "axios";
 
 export default function Inputs() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [mail, setMail] = useState("");
-  const [number, setNumber] = useState("");
-  const [textarea, setTextarea] = useState("");
-  const [button, setButton] = useState(false);
-  const [message, setMessage] = useState("");
-  const [messageVisibility, setMessageVisibility] = useState("hidden");
-
-  console.log(mail);
-
-  function handleSubmit() {
-    const regEx =
-      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (regEx.test(mail)) {
-      axios
-        .post("/api/waitlist", {
-          firstName: firstName,
-          lastName: lastName,
-          email: mail,
-          phoneNumber: number,
-          message: textarea,
-        })
-        .then(() => {
-          console.log("success");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      setMessageVisibility("hidden");
-    } else {
-      setMessageVisibility("block");
-      setMessage("Enter an appropriate email");
-    }
-  }
-
   return (
     <>
       <main className="flex flex-col gap-y-4 lg:gap-y-8 lg:pt-14">
@@ -57,8 +21,6 @@ export default function Inputs() {
               className="w-40 pl-2 py-2 lg:w-max capitalize placeholder:text-[#667085] lg:p-3.5 lg:pr-20 outline-none text-lg font-normal rounded-lg"
               type="name"
               placeholder="First name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
             />
             <h1 className="text-sm text-[#eed600] hidden">
               Don’t forget to fill this
@@ -70,8 +32,6 @@ export default function Inputs() {
               className="w-40 py-2 pl-2 lg:w-max capitalize placeholder:text-[#667085] lg:p-3.5 lg:pr-20 outline-none text-lg font-normal rounded-lg"
               type="name"
               placeholder="Last name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
             />
             <h1 className="text-sm text-[#eed600] hidden">
               Don’t forget to fill this
@@ -84,11 +44,9 @@ export default function Inputs() {
             className="w-full placeholder:text-[#667085] p-3.5 outline-none text-lg font-normal rounded-lg"
             type="email"
             placeholder="you@company.com"
-            value={mail}
-            onChange={(e) => setMail(e.target.value)}
           />
-          <h1 className={`text-sm text-[#eed600] ${messageVisibility}`}>
-            {message}
+          <h1 className="text-sm text-[#eed600] hidden">
+            Don’t forget to fill this
           </h1>
         </div>
         <div>
@@ -115,8 +73,6 @@ export default function Inputs() {
               id="phone-number"
               className="pl-20 w-full placeholder:text-[#667085] p-3.5 outline-none text-lg font-normal rounded-lg"
               placeholder="+1 (555) 987-6543"
-              value={number}
-              onChange={(e) => setNumber(e.target.value)}
             />
           </div>
         </div>
@@ -125,11 +81,9 @@ export default function Inputs() {
           <textarea
             className="placeholder:text-[#667085] outline-none text-lg font-normal rounded-lg w-full h-36 pl-2 pt-2"
             placeholder="What’s on your mind?"
-            onChange={(e) => setTextarea(e.target.value)}
           />
         </div>
         <button
-          onClick={handleSubmit}
           type="submit"
           className="w-full py-3 rounded-lg bg-[#cc5900] text-white hover:bg-white hover:text-[#cc5900] transition-all text-lg font-semibold"
         >
